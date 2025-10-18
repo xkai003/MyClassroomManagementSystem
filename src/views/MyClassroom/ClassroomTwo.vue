c04fd4470bb19f40cdac849ca5dafa27fdca7e3e
xkai003 <xkai03@outlook.com>
2025-10-18 14:17:49 +0800

    two

--- src/views/MyClassroom/ClassroomTwo.vue
+++ src/views/MyClassroom/ClassroomTwo.vue
@@ -1,30 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="box">
-    <div class="ckgd">
-      <div class="one" v-for="item in list" :key="item.id">
-        <div class="left">
-          <p>{{ item.course }}</p>
-            <table>
-              <tr>
-                <td>上课时间：</td>
-                <td>{{ item.date }}</td>
-              </tr>
-              <tr>
-                <td>所属课程：</td>
-                <td>{{ item.course }}</td>
-              </tr>
-              <tr>
-                <td>地址：</td>
-                <td>{{ item.location }}</td>
-              </tr>
-            </table>
-        </div>
-        <div class="right">
-            <a :href=item.url>进入</a>
-            <button @click="del(item.id)">删除</button>
-        </div>
-=======
   <div class="box bg-gray-50 min-h-screen p-4 sm:p-8 font-sans">
     <div class="one">
       <div class="input">
@@ -65,7 +39,6 @@
             <button type="button" @click="resetForm()">重置</button>
           </div>
         </form>
->>>>>>> 11f108f0819b7462d86fd18fc44e952502edd469
       </div>
     </div>
   </div>
@@ -73,32 +46,6 @@
 
 <script>
 import axios from 'axios'
-<<<<<<< HEAD
-export default {
-  data () {
-    return {
-      list: [
-        {
-          id: 1,
-          date: '0',
-          course: '0',
-          location: '0',
-          url: '0'
-        }
-      ]
-    }
-  },
-  async created () {
-    try {
-      // 1、先 axios 请求并后端MySQL的数据
-      const res = await axios.get('https://gist.githubusercontent.com/xkai003/dffaa48ea67e287c8b49cb918cfa9a4b/raw/6e3f97cc81fcb5131cec5c864bcd91a6b1a7a227/MyClassroomManagementSystem.json')
-      console.log('ClassroomTwo部分请求数据：', res.data.myclassroom)
-      // 2、把请求过来的数据传给 list 数组
-      this.list = res.data.myclassroom
-    } catch (error) {
-      console.error('请求失败', error)
-    }
-=======
 
 // ⚠️ 安全警告：客户端存储token不安全
 // 在生产环境中应该使用后端服务
@@ -278,7 +225,6 @@ export default {
   // 组件创建时加载数据
   async mounted () {
     await this.fetchClasses()
->>>>>>> 11f108f0819b7462d86fd18fc44e952502edd469
   }
 }
 </script>
